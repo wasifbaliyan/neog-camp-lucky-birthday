8ac468563d839c88e14be4015bc85b52e3f1d321
Wasif Baliyan <w.baliyan@gmail.com>
2020-12-22 19:08:09 +0530

    first commit and finished all the fucntionality of the app

---
+++ README.md
@@ -0,0 +1,7 @@
+# Is your birthday lucky? Check it out...
+
+This app take users birthday and lucky number as input and calculate if the sum of all digits in their birthday is exactly divisible by lucky number. If yes, then Lucky else not lucky.
+
+> I add model to this app for privacy notice so please wait for 5 seconds for model to show.
+
+I use vanilla JS, css and html5 for this app.
---
+++ js/app.js
@@ -0,0 +1,45 @@
+const luckyNum = document.querySelector("#lucky-num");
+const bday = document.querySelector("#bday");
+const form = document.querySelector(".form");
+const section = document.querySelector("section");
+const result = document.querySelector(".result");
+const model = document.querySelector(".model");
+const modelBtn = document.querySelector(".model-btn");
+const img = document.querySelector(".img");
+img.style.display = "none";
+model.style.display = "none";
+
+setTimeout(() => {
+  model.style.display = "";
+}, 5000);
+
+modelBtn.addEventListener("click", () => {
+  model.style.display = "none";
+});
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  const dateSum = bday.value.split("-").reduce((a, c) => {
+    return Number(a) + Number(c);
+  }, 0);
+  const luckyNumber = Number(luckyNum.value);
+
+  const areYouLucky = dateSum % luckyNumber === 0 ? true : false;
+  console.log(areYouLucky);
+  luckyNum.value = "";
+  bday.value = "";
+
+  section.classList.add("lucky-section");
+  if (areYouLucky) {
+    result.textContent = "Congratulations, You're lucky person.";
+    img.style.display = "block";
+    img.src = "/assets/happy.svg";
+  } else {
+    result.textContent = `OHHH! You're ${
+      dateSum % luckyNumber
+    } days away from being lucky.`;
+    img.style.display = "block";
+    img.src = "/assets/sad.svg";
+  }
+});
